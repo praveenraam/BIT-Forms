061c1e56979b03188f190715b94c87f29394cbb0
premnath018 <prem185nath@gmail.com>
2023-07-06 00:43:51 +0530

    'Added Moodle Form B'

---
+++ forms/curriculum.js
@@ -0,0 +1,60 @@
+const curriculum = {
+    "Departments": {
+      "Information Technology": {
+        "I Year": {
+          "SEM-2": [
+            {
+              "code": "22MA201",
+              "course": "ENGINEERING MATHEMATICS II"
+            },
+            {
+              "code": "22PH202",
+              "course": "ELECTROMAGNETISM AND MODERN PHYSICS"
+            },
+            {
+              "code": "22CH203",
+              "course": "ENGINEERING CHEMISTRY II"
+            },
+            {
+              "code": "22GE003",
+              "course": "BASICS OF ELECTRICAL ENGINEERING"
+            },
+            {
+              "code": "22GE002",
+              "course": "COMPUTATIONAL PROBLEM SOLVING"
+            },
+            {
+              "code": "22IT206",
+              "course": "DIGITAL COMPUTER ELECTRONICS"
+            }
+          ],
+          "SEM-3": [
+            {
+              "code": "22IT311",
+              "course": "Probability, Statistics and Queuing Theory"
+            },
+            {
+              "code": "22IT312",
+              "course": "Data Structures I"
+            },
+            {
+              "code": "22IT313",
+              "course": "Computer Organization and Architecture"
+            },
+            {
+              "code": "22IT314",
+              "course": "Principles of Programming Languages"
+            },
+            {
+              "code": "22IT315",
+              "course": "Software Engineering"
+            },
+            {
+              "code": "22HS004",
+              "course": "Human Values and Ethics"
+            }
+          ]
+        }
+      }
+    }
+  }
