0b87a0dccd496c822c2c3d37542a8892b339fb18
PraveenRaamC <ckpraveenraam@gmail.com>
2023-07-05 10:33:34 +0530

    Files were added

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // Do not cache
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BIT-Forms</title>
+    <!--Google Font-->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="style.css?v=2">
+<link rel="icon" type="image/x-icon" href="bit-fa.ico">
+<meta http-equiv='cache-control' content='no-cache'>
+<meta http-equiv='expires' content='0'>
+<meta http-equiv='pragma' content='no-cache'>
+</head>
+<body>
+    <div id="totalcont">
+        <div class="title-con">
+            <h3 class="title">BIT Forms</h3>
+        </div>
+        <div class="cards">
+            <div class="card">
+                <div class="container">
+                    <img class="image" src="bit.png" alt="img">
+                </div>
+                <div class="details">
+                    <h2>MOODLE</h2>
+                    <p>FA Unblock Form</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <img class="image" src="bit.png" alt="img">
+                </div>
+                <div class="details">
+                    <h2>MOODLE</h2>
+                    <p>Account Revoke Form</p>    
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <img class="image" src="bit.png" alt="img">
+                </div>
+                <div class="details">
+                    <h2>OD FORM</h2>
+                    <p>Event & Other</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <img class="image" src="bit.png" alt="img">
+                </div>
+                <div class="details">
+                    <h2>OD FORM</h2>
+                    <p>Internship</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="container">
+                    <img class="image" src="bit.png" alt="imge">
+                </div>
+                <div class="details">
+                    <h2>ATTENDENCE</h2>
+                    <p>Unblock and Security clearance form</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
